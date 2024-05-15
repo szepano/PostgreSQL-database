2b9c6b45424503e2b045da5002b38981b6b9fbe3
szepano <156124248+szepano@users.noreply.github.com>
2024-05-16 00:33:51 +0200

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-from base import Student, Group, Teacher, Subject, Grade, session
-from sqlalchemy import select
-
-
-
-if __name__ == '__main__':
-    student = select(Student)
-    result = session.execute(student)
-    print(result.all())
